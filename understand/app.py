8a95d4a150ce91cbbfecbedb1ec00336871111af
Ryan Johnson <10009758+ryjohnson09@users.noreply.github.com>
2025-02-25 16:27:16 -0500

    Update app.py

--- understand/app.py
+++ understand/app.py
@@ -3,7 +3,7 @@ import pins
 
 pin_model_name = "____"
 
-b = pins.board_connect(server_url='https://pub.ferryland.posit.team/', allow_pickle_read=True)
+b = pins.board_connect(server_url='https://pub.workshop.posit.team/', allow_pickle_read=True)
 v = VetiverModel.from_pin(b, pin_model_name)
 
 vetiver_api = VetiverAPI(v)
