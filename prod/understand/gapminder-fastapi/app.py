447e0a913c1e4b1b11f2fca6f996726600299543
Ryan Johnson <10009758+ryjohnson09@users.noreply.github.com>
2025-02-26 14:35:39 -0500

    Update app.py

--- prod/understand/gapminder-fastapi/app.py
+++ prod/understand/gapminder-fastapi/app.py
@@ -1,7 +1,7 @@
 from vetiver import VetiverModel, VetiverAPI
 import pins
 
-b = pins.board_connect(server_url='https://pub.ferryland.posit.team/', allow_pickle_read=True)
+b = pins.board_connect(server_url='https://pub.workshop.posit.team/', allow_pickle_read=True)
 v = VetiverModel.from_pin(b, 'ryjohnson09/gapminder_model_rf')
 
 vetiver_api = VetiverAPI(v)
