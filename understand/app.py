a5dbffc38b0c887fcd9d88e3eeae73050f615e1d
Ryan Johnson <10009758+ryjohnson09@users.noreply.github.com>
2025-07-26 15:23:34 -0500

    Update app.py

--- understand/app.py
+++ understand/app.py
@@ -1,7 +1,7 @@
 from vetiver import VetiverModel, VetiverAPI
 import pins
 
-pin_model_name = "____"
+pin_model_name = "ryjohnson09/gapminder_model_rf"
 
 b = pins.board_connect(server_url='https://pub.workshop.posit.team/', allow_pickle_read=True)
 v = VetiverModel.from_pin(b, pin_model_name)
